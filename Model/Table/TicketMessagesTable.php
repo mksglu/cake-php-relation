8673ebf119767175d2d03d24ffcd9119772ad156
Mert Köseoğlu <info@mkoseoglu.com>
2018-01-18 03:26:39 +0300

    UsersComplete

--- Model/Table/TicketMessagesTable.php
+++ Model/Table/TicketMessagesTable.php
@@ -3,33 +3,21 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
-// use Cake\Validation\Validator;
-
+ 
 class TicketMessagesTable extends Table
 {
-    public   function initialize(array $config)
+    public function initialize(array $config)
     {
-
         parent::initialize($config);
-/*
-        $this->table('ticket_messages');
-        $this->displayField('id');
-        $this->primaryKey('id');
-        $this->addBehavior('Timestamp');
-        $this->belongsTo('Tickets', [  //ManyToOne belongsTo
-             'foreignKey' => 'id',  
-             'joinType' => 'INNER'        
-        ]);https://www.youtube.com/watch?v=nOjyUm4G2Bc
-*/
-$this->table('ticket_messages');
-$this->displayField('id');
-$this->primaryKey('id');
-$this->addBehavior('Timestamp');
-$this->belongsTo('Tickets', [
-    'foreignKey' => 'ticket_id',
-    'joinType' => 'INNER'    
-]);
-     
+ 
+		$this->table('ticket_messages');
+		$this->displayField('id');
+		$this->primaryKey('id');
+		$this->addBehavior('Timestamp');
+		$this->belongsTo('Tickets', [
+		    'foreignKey' => 'ticket_id',
+		    'joinType' => 'INNER'    
+		]);
  
     }
    
